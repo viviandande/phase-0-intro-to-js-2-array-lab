359f70d0bd28ce61d3eb4a64ad4dbcd4fab6dff8
dande-vivian <118072739+viviandande@users.noreply.github.com>
2023-06-27 02:32:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,36 @@
 // Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"];
+const cats = ["Milo","Otis", "Garfield"]
 
-
-//push() used to add an item to the end of an array
 function destructivelyAppendCat(name){
-    cats.push('Ralph');
+    cats.push("Ralph")
 }
-cats.length = 0;
-cats.push('Milo', 'Otis', 'Garfield');
 
-//destructivelyAppendCat();
-//console.log(cats)
-
-// unshift() method adds one or more elements to the
-//beginning of an array and returns the new length of the array.
 function destructivelyPrependCat(name){
-    cats.unshift('Bob')
+    cats.unshift("Bob")
 }
 
-cats.length = 0;
-cats.push('Milo', 'Otis', 'Garfield');
-
-
-//pop() method removes the last element from an array and
-//returns that value to the caller
 function destructivelyRemoveLastCat(){
-    cats.pop();
+    cats.pop([cats.length - 1])
 }
-cats.length = 0;
-cats.push('Milo', 'Otis', 'Garfield');
 
-//shift() method removes the element at the zeroth index and
-//shifts the values at consecutive indexes down, then returns the removed value.
 function destructivelyRemoveFirstCat(){
-    cats.shift()
+    cats.shift(0)
 }
-//cats.length = 0;
-//cats.push('Milo', 'Otis', 'Garfield');
-
-//appends a cat to the cats array and returns a new array, leaving the cats array unchanged
 
 function appendCat(name){
-    let copyOfCat = [...cats, "Broom"]
-    return copyOfCat
+    const addCatLast = [...cats, "Broom"]
+    return addCatLast
 }
-
-
 function prependCat(name){
-    let prepEnd = ["Arnold", ...cats]
-    return prepEnd
+    const addCatFirst = ["Arnold", ...cats]
+    return addCatFirst
 }
-
 
 function removeLastCat(){
-    let removeLast = cats.slice(0,2)
+    const removeLast = cats.slice(0,2)
     return removeLast
-
 }
-function removeFirstCat(){
-    let removeFirst = cats.slice(1)
+function  removeFirstCat(){
+    const removeFirst = cats.slice(1)
     return removeFirst
-
 }
